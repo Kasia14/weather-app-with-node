43db021f2ea2427d958819d3fecd97a5ff5bc63c
Katarzyna <55921691+Kasia14@users.noreply.github.com>
2021-02-11 13:57:13 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const https = require("https");
+const port = 3000
+const app = express();
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+
+})
+
+app.post("/", (req, res) =>{
+   
+    const query = req.body.cityName;
+    const key = "eb3e9c8adfb947defbed4707fc06351a";
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid="+key+"&units=" + unit;
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imgURL = "http://openweathermap.org/img/wn/"+icon+ "@2x.png";
+            res.write("<p>The weather is currently " + description + "</p>");
+            res.write("<h1>The temperature in"+ query+ " is " + temp + " degrees Celcius. </h1>" );
+            res.write("<img src=" + imgURL + ">");
+            res.send()
+        })
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
